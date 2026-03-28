d0f66927eb3bee0bb0e9d8a9f0e7b646383e0a1f
Tom Schuster <tschuster@mozilla.com>
2026-03-28 22:11:45 +0000

    Bug 2025332 - ErrorResult should call ClearUnionData more.  a=diannaS
    Original Revision: https://phabricator.services.mozilla.com/D289843
    
    Differential Revision: https://phabricator.services.mozilla.com/D290474

--- dom/bindings/ErrorResult.h
+++ dom/bindings/ErrorResult.h
@@ -211,6 +211,7 @@ class TErrorResult {
   // informative message and calling the relevant Throw*Error.
   void MOZ_MUST_RETURN_FROM_CALLER_IF_THIS_IS_ARG Throw(nsresult rv) {
     MOZ_ASSERT(NS_FAILED(rv), "Please don't try throwing success");
+    ClearUnionData();
     AssignErrorCode(rv);
   }
 
@@ -427,7 +428,10 @@ class TErrorResult {
   // Backwards-compat to make conversion simpler.  We don't call
   // Throw() here because people can easily pass success codes to
   // this.  This operator is deprecated and ideally shouldn't be used.
-  void operator=(nsresult rv) { AssignErrorCode(rv); }
+  void operator=(nsresult rv) {
+    ClearUnionData();
+    AssignErrorCode(rv);
+  }
 
   bool Failed() const { return NS_FAILED(mResult); }
 
@@ -538,6 +542,7 @@ class TErrorResult {
   }
 
   void AssignErrorCode(nsresult aRv) {
+    MOZ_ASSERT(mUnionState == HasNothing);
     MOZ_ASSERT(aRv != NS_ERROR_INTERNAL_ERRORRESULT_TYPEERROR,
                "Use ThrowTypeError()");
     MOZ_ASSERT(aRv != NS_ERROR_INTERNAL_ERRORRESULT_RANGEERROR,
