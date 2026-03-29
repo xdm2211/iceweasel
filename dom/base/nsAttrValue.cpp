b034dca2ffac00512473ed34849651192ce1885a
Markus Stange <mstange.moz@gmail.com>
2026-03-29 03:25:22 +0000

    Bug 2026778 - Add missing ResetIfSet() so that we don't leak the existing MiscContainer, if present.  a=diannaS
    Original Revision: https://phabricator.services.mozilla.com/D290265
    
    Differential Revision: https://phabricator.services.mozilla.com/D290539

--- dom/base/nsAttrValue.cpp
+++ dom/base/nsAttrValue.cpp
@@ -1332,7 +1332,11 @@ void nsAttrValue::ParseAtom(const nsAString& aValue) {
 void nsAttrValue::ParseAtomArray(nsAtom* aValue) {
   if (MiscContainer* cont = AtomArrayCache::Lookup(aValue)) {
     // Set our MiscContainer to the cached one.
+    // AddRef must happen before ResetIfSet: the cache does not hold a strong
+    // reference, and ResetIfSet could release the last reference to cont if
+    // this nsAttrValue is already holding it.
     NS_ADDREF(cont);
+    ResetIfSet();
     SetPtrValueAndType(cont, eOtherBase);
     return;
   }
@@ -1932,7 +1936,11 @@ bool nsAttrValue::ParseStyleAttribute(const nsAString& aString,
   if (cachingAllowed) {
     if (MiscContainer* cont = attrStyles->LookupStyleAttr(aString)) {
       // Set our MiscContainer to the cached one.
+      // AddRef must happen before ResetIfSet: the cache does not hold a strong
+      // reference, and ResetIfSet could release the last reference to cont if
+      // this nsAttrValue is already holding it.
       NS_ADDREF(cont);
+      ResetIfSet();
       SetPtrValueAndType(cont, eOtherBase);
       return true;
     }
