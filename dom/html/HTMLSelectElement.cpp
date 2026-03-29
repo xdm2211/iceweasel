730398a17d5d70f2fccfef8c30023c5fc55cce51
Emilio Cobos Álvarez <emilio@crisal.io>
2026-03-29 03:21:47 +0000

    Bug 2025402. a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D289495
    
    Differential Revision: https://phabricator.services.mozilla.com/D290415

--- dom/html/HTMLSelectElement.cpp
+++ dom/html/HTMLSelectElement.cpp
@@ -345,23 +345,9 @@ void HTMLSelectElement::InsertOptionsIntoList(nsIContent* aOptions,
       OnSelectionChanged();
     }
 
-    // Get the frame stuff for notification. No need to flush here
-    // since if there's no frame for the select yet the select will
-    // get into the right state once it's created.
-    nsListControlFrame* listBoxFrame = nullptr;
-    AutoWeakFrame weakSelectFrame;
-    bool didGetFrame = false;
-
     // Actually select the options if the added options warrant it
     for (int32_t i = aListIndex; i < insertIndex; i++) {
-      // Notify the frame that the option is added
-      if (!didGetFrame || (listBoxFrame && !weakSelectFrame.IsAlive())) {
-        listBoxFrame = GetListBoxFrame();
-        weakSelectFrame = do_QueryFrame(listBoxFrame);
-        didGetFrame = true;
-      }
-
-      if (listBoxFrame) {
+      if (auto* listBoxFrame = GetListBoxFrame()) {
         listBoxFrame->AddOption(i);
       }
 
@@ -378,7 +364,7 @@ void HTMLSelectElement::InsertOptionsIntoList(nsIContent* aOptions,
         // This is sort of a hack ... we need to notify that the option was
         // set and change selectedIndex even though we didn't really change
         // its value.
-        OnOptionSelected(listBoxFrame, i, true, false, aNotify);
+        OnOptionSelected(i, true, false, aNotify);
       }
     }
 
@@ -758,8 +744,7 @@ bool HTMLSelectElement::IsOptionSelectedByIndex(int32_t aIndex) const {
   return option && option->Selected();
 }
 
-void HTMLSelectElement::OnOptionSelected(nsListControlFrame* aSelectFrame,
-                                         int32_t aIndex, bool aSelected,
+void HTMLSelectElement::OnOptionSelected(int32_t aIndex, bool aSelected,
                                          bool aChangeOptionState,
                                          bool aNotify) {
   // Set the selected index
@@ -779,8 +764,8 @@ void HTMLSelectElement::OnOptionSelected(nsListControlFrame* aSelectFrame,
   }
 
   // Let the frame know too
-  if (aSelectFrame) {
-    aSelectFrame->OnOptionSelected(aIndex, aSelected);
+  if (auto* listBox = GetListBoxFrame()) {
+    listBox->OnOptionSelected(aIndex, aSelected);
   }
 
 #ifdef ACCESSIBILITY
@@ -857,10 +842,6 @@ bool HTMLSelectElement::SetOptionsSelectedByIndex(int32_t aStartIndex,
   bool optionsSelected = false;
   bool optionsDeselected = false;
 
-  nsListControlFrame* listBoxFrame = nullptr;
-  bool didGetFrame = false;
-  AutoWeakFrame weakSelectFrame;
-
   if (aOptionsMask.contains(OptionFlag::IsSelected)) {
     // Setting selectedIndex to an out-of-bounds index means -1. (HTML5)
     if (aStartIndex < 0 || AssertedCast<uint32_t>(aStartIndex) >= numItems ||
@@ -910,15 +891,7 @@ bool HTMLSelectElement::SetOptionsSelectedByIndex(int32_t aStartIndex,
         // the option has just been inserted we have to get in sync with it.
         if (option && (aOptionsMask.contains(OptionFlag::InsertingOptions) ||
                        !option->Selected())) {
-          // To notify the frame if anything gets changed. No need
-          // to flush here, if there's no frame yet we don't need to
-          // force it to be created just to notify it about a change
-          // in the select.
-          listBoxFrame = GetListBoxFrame();
-          weakSelectFrame = do_QueryFrame(listBoxFrame);
-          didGetFrame = true;
-
-          OnOptionSelected(listBoxFrame, optIndex, true, !option->Selected(),
+          OnOptionSelected(optIndex, true, !option->Selected(),
                            aOptionsMask.contains(OptionFlag::Notify));
           optionsSelected = true;
         }
@@ -938,17 +911,7 @@ bool HTMLSelectElement::SetOptionsSelectedByIndex(int32_t aStartIndex,
           HTMLOptionElement* option = Item(optIndex);
           // If the index is already deselected, ignore it.
           if (option && option->Selected()) {
-            if (!didGetFrame || (listBoxFrame && !weakSelectFrame.IsAlive())) {
-              // To notify the frame if anything gets changed, don't
-              // flush, if the frame doesn't exist we don't need to
-              // create it just to tell it about this change.
-              listBoxFrame = GetListBoxFrame();
-              weakSelectFrame = do_QueryFrame(listBoxFrame);
-
-              didGetFrame = true;
-            }
-
-            OnOptionSelected(listBoxFrame, optIndex, false, true,
+            OnOptionSelected(optIndex, false, true,
                              aOptionsMask.contains(OptionFlag::Notify));
             optionsDeselected = true;
 
@@ -972,17 +935,7 @@ bool HTMLSelectElement::SetOptionsSelectedByIndex(int32_t aStartIndex,
 
       // If the index is already selected, ignore it.
       if (option && option->Selected()) {
-        if (!didGetFrame || (listBoxFrame && !weakSelectFrame.IsAlive())) {
-          // To notify the frame if anything gets changed, don't
-          // flush, if the frame doesn't exist we don't need to
-          // create it just to tell it about this change.
-          listBoxFrame = GetListBoxFrame();
-          weakSelectFrame = do_QueryFrame(listBoxFrame);
-
-          didGetFrame = true;
-        }
-
-        OnOptionSelected(listBoxFrame, optIndex, false, true,
+        OnOptionSelected(optIndex, false, true,
                          aOptionsMask.contains(OptionFlag::Notify));
         optionsDeselected = true;
       }
