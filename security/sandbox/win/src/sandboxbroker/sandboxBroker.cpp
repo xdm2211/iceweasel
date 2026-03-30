e39358a84208df5d76134953bd90e3ac63e54ef5
Bob Owen <bobowencode@gmail.com>
2026-03-30 17:57:36 +0000

    Bug 2026959 - Fix Trellix exclusion check for closing KsecDD.  a=diannaS
    Original Revision: https://phabricator.services.mozilla.com/D290458
    
    Differential Revision: https://phabricator.services.mozilla.com/D290803

--- security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
+++ security/sandbox/win/src/sandboxbroker/sandboxBroker.cpp
@@ -1124,7 +1124,7 @@ void SandboxBroker::SetSecurityLevelForContentProcess(int32_t aSandboxLevel,
 #else
     isTrellixDllLoaded = !!::GetModuleHandleW(L"fcagff.dll");
 #endif
-    if (isTrellixDllLoaded) {
+    if (!isTrellixDllLoaded) {
       result = config->AddKernelObjectToClose(L"File", L"\\Device\\KsecDD");
       MOZ_RELEASE_ASSERT(sandbox::SBOX_ALL_OK == result,
                          "AddKernelObjectToClose should never fail.");
