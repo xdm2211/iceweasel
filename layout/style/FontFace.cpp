42c5518076313ed0c3f5bf8181d8ae47891a4dd5
Emilio Cobos Álvarez <emilio@crisal.io>
2026-03-31 10:13:47 +0000

    Bug 2026299 - Improve FontFace promise resolution + followups.  a=dmeehan DONTBUILD
    Make promise resolution async, and a lot of the FontFaceSetImpl stuff
    doesn't make sense anymore because FontFaceSetImpl _is_ thread-safe.
    
    Original Revision: https://phabricator.services.mozilla.com/D289918
    
    Differential Revision: https://phabricator.services.mozilla.com/D290414

--- layout/style/FontFace.cpp
+++ layout/style/FontFace.cpp
@@ -248,6 +248,7 @@ Promise* FontFace::Load(ErrorResult& aRv) {
   }
 
   mImpl->Load(aRv);
+  mImpl->UpdateOwnerKeepAlive();
   return mLoaded;
 }
 
@@ -259,40 +260,27 @@ Promise* FontFace::GetLoaded(ErrorResult& aRv) {
     return nullptr;
   }
 
+  if (mImpl) {
+    mImpl->UpdateOwnerKeepAlive();
+  }
+
   return mLoaded;
 }
 
 void FontFace::MaybeResolve() {
   gfxFontUtils::AssertSafeThreadOrServoFontMetricsLocked();
+  MOZ_ASSERT(!NS_IsMainThread() || nsContentUtils::IsSafeToRunScript());
+  MOZ_ASSERT(!gfxFontUtils::CurrentServoStyleSet());
 
-  if (!mLoaded) {
-    return;
+  if (RefPtr loaded = mLoaded) {
+    loaded->MaybeResolve(this);
   }
-
-  if (ServoStyleSet* ss = gfxFontUtils::CurrentServoStyleSet()) {
-    // See comments in Gecko_GetFontMetrics.
-    ss->AppendTask(PostTraversalTask::ResolveFontFaceLoadedPromise(this));
-    return;
-  }
-
-  if (NS_IsMainThread() && !nsContentUtils::IsSafeToRunScript()) {
-    nsContentUtils::AddScriptRunner(NewRunnableMethod(
-        "FontFace::MaybeResolve", this, &FontFace::MaybeResolve));
-    return;
-  }
-
-  mLoaded->MaybeResolve(this);
 }
 
 void FontFace::MaybeReject(nsresult aResult) {
   gfxFontUtils::AssertSafeThreadOrServoFontMetricsLocked();
-
-  if (ServoStyleSet* ss = gfxFontUtils::CurrentServoStyleSet()) {
-    // See comments in Gecko_GetFontMetrics.
-    ss->AppendTask(
-        PostTraversalTask::RejectFontFaceLoadedPromise(this, aResult));
-    return;
-  }
+  MOZ_ASSERT(!NS_IsMainThread() || nsContentUtils::IsSafeToRunScript());
+  MOZ_ASSERT(!gfxFontUtils::CurrentServoStyleSet());
 
   if (mLoaded) {
     mLoaded->MaybeReject(aResult);
