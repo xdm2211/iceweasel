ccba7cfbff3ed30404174aa565d1ecc324240cc0
Alastor Wu <alwu@mozilla.com>
2026-04-02 20:45:26 +0000

    Bug 2025502 - Serialize MediaSystemResourceService shutdown on the compositor thread.  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D289873
    
    Differential Revision: https://phabricator.services.mozilla.com/D290954

--- dom/media/systemservices/MediaSystemResourceService.h
+++ dom/media/systemservices/MediaSystemResourceService.h
@@ -24,7 +24,7 @@ class MediaSystemResourceService {
  public:
   NS_INLINE_DECL_THREADSAFE_REFCOUNTING(MediaSystemResourceService)
 
-  static MediaSystemResourceService* Get();
+  static already_AddRefed<MediaSystemResourceService> Get();
   static void Init();
   static void Shutdown();
 
