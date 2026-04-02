2d1421a3c4eae3aa853faf01ce7f083eba810565
Randell Jesup <rjesup@mozilla.com>
2026-04-02 17:43:14 +0000

    Bug 2024661: Clean up DeleteChannel  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D288712
    
    Differential Revision: https://phabricator.services.mozilla.com/D290590

--- netwerk/protocol/http/BackgroundChannelRegistrar.cpp
+++ netwerk/protocol/http/BackgroundChannelRegistrar.cpp
@@ -32,7 +32,7 @@ BackgroundChannelRegistrar::~BackgroundChannelRegistrar() {
 }
 
 // static
-already_AddRefed<nsIBackgroundChannelRegistrar>
+already_AddRefed<BackgroundChannelRegistrar>
 BackgroundChannelRegistrar::GetOrCreate() {
   if (!gSingleton) {
     gSingleton = new BackgroundChannelRegistrar();
@@ -55,8 +55,22 @@ void BackgroundChannelRegistrar::NotifyChannelLinked(
 void BackgroundChannelRegistrar::DeleteChannel(uint64_t aKey) {
   MOZ_ASSERT(NS_IsMainThread());
 
-  mChannels.Remove(aKey);
-  mBgChannels.Remove(aKey);
+  RefPtr<HttpChannelParent> channel;
+  mChannels.Remove(aKey, getter_AddRefs(channel));
+  RefPtr<HttpBackgroundChannelParent> bgChannel;
+  mBgChannels.Remove(aKey, getter_AddRefs(bgChannel));
+}
+
+void BackgroundChannelRegistrar::DeleteChannelIfMatches(
+    uint64_t aKey, HttpChannelParent* aExpected) {
+  MOZ_ASSERT(NS_IsMainThread());
+
+  RefPtr<HttpChannelParent> channel;
+  if (mChannels.GetWeak(aKey) == aExpected) {
+    mChannels.Remove(aKey, getter_AddRefs(channel));
+  }
+  RefPtr<HttpBackgroundChannelParent> bgChannel;
+  mBgChannels.Remove(aKey, getter_AddRefs(bgChannel));
 }
 
 void BackgroundChannelRegistrar::LinkHttpChannel(uint64_t aKey,
