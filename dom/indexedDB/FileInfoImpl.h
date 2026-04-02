9e78027800b93dde67cae46d48a3580e1f2b236b
Harveer Singh <hsingh@mozilla.com>
2026-04-02 15:41:38 +0000

    Bug 2025401: Fixing a bug in IDB blob files refcouting where stale count could carried over across savepoints..  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D290463
    
    Differential Revision: https://phabricator.services.mozilla.com/D291758

--- dom/indexedDB/FileInfoImpl.h
+++ dom/indexedDB/FileInfoImpl.h
@@ -71,7 +71,7 @@ void FileInfo<FileManager>::UpdateReferences(ThreadSafeAutoRefCnt& aRefCount,
 
     aRefCount = aRefCount + aDelta;
 
-    if (mRefCnt + mDBRefCnt > 0) {
+    if (mRefCnt > 0 || mDBRefCnt > 0) {
       return;
     }
 
