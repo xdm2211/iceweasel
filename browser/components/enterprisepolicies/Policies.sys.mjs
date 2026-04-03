adcc4b8c2084d63fab9e0313d138ab54012b96a2
Dana Keeler <dkeeler@mozilla.com>
2026-04-03 08:48:24 +0000

    Bug 2027888 - undo enterprise policies async changes from bug 2014517  a=pascalc DONTBUILD
    This should restore the enterprise policies implementation to the non-async
    implementation from before bug 2014517. It also adds a test for
    SecurityDevices.
    
    Original Revision: https://phabricator.services.mozilla.com/D291064
    
    Differential Revision: https://phabricator.services.mozilla.com/D291787

--- browser/components/enterprisepolicies/Policies.sys.mjs
+++ browser/components/enterprisepolicies/Policies.sys.mjs
@@ -2837,7 +2837,7 @@ export var Policies = {
   },
 
   SecurityDevices: {
-    async onProfileAfterChange(manager, param) {
+    async _onProfileAfterChangeImpl(manager, param) {
       let pkcs11db = Cc["@mozilla.org/security/pkcs11moduledb;1"].getService(
         Ci.nsIPKCS11ModuleDB
       );
@@ -2886,6 +2886,20 @@ export var Policies = {
           lazy.log.debug(ex);
         }
       }
+    },
+
+    onProfileAfterChange(manager, param) {
+      this._onProfileAfterChangeImpl(manager, param)
+        .then(() => {
+          Services.obs.notifyObservers(
+            null,
+            "test-enterprisepolicies-securitydevices"
+          );
+        })
+        .catch(ex => {
+          lazy.log.error(`Error running SecurityDevices.onProfileAfterChange`);
+          lazy.log.debug(ex);
+        });
     },
   },
 
