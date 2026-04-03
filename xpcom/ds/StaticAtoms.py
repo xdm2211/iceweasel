7e1bf696a9c96ec38fa374cc3fddb2851e154d92
Emilio Cobos Álvarez <emilio@crisal.io>
2026-04-03 18:22:06 +0000

    Bug 2024864 - Gate @appearance-base sheet behind a pref.  a=diannaS DONTBUILD
    I believe bug 2028657 (try run in progress) should fix the main root
    cause, but this would be useful and give us more time to investigate
    otherwise.
    
    Original Revision: https://phabricator.services.mozilla.com/D291610
    
    Differential Revision: https://phabricator.services.mozilla.com/D291994

--- xpcom/ds/StaticAtoms.py
+++ xpcom/ds/StaticAtoms.py
@@ -2464,6 +2464,7 @@ STATIC_ATOMS = [
     Atom("mathml_font_family_math_enabled", "mathml.font_family_math.enabled"),
     Atom("layout_css_always_underline_links", "layout.css.always_underline_links"),
     Atom("layout_css_android_pip_enabled", "layout.css.android-pip.enabled"),
+    Atom("layout_css_appearance_base_enabled", "layout.css.appearance-base.enabled"),
     Atom("layout_css_cached_scrollbar_styles_enabled", "layout.css.cached-scrollbar-styles.enabled"),
     Atom("layout_testing_scrollbars_always_hidden", "layout.testing.scrollbars.always-hidden"),
     # Contextual Identity / Containers
