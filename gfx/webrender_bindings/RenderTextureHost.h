7377d376f6af7885fa3a15cb7b13c8a82c4f7e29
Lee Salzman <lsalzman@mozilla.com>
2026-04-03 03:10:48 +0000

    Bug 2027359. r=sotaro,gfx-reviewers, a=dsmith DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D291395

--- gfx/webrender_bindings/RenderTextureHost.h
+++ gfx/webrender_bindings/RenderTextureHost.h
@@ -95,6 +95,11 @@ class RenderTextureHost {
 
   virtual void UnlockSWGL() {}
 
+  virtual bool LockSWGLCompositeSurface(void* aContext,
+                                        wr::SWGLCompositeSurfaceInfo* aInfo) {
+    return false;
+  }
+
   virtual RefPtr<layers::TextureSource> CreateTextureSource(
       layers::TextureSourceProvider* aProvider);
 
