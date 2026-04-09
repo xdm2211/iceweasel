801dc7caf7b54842c8337122bce32635174d2048
Emilio Cobos Álvarez <emilio@crisal.io>
2026-04-09 16:25:17 +0000

    Bug 2029283 - Update OTS.  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D292076
    
    Differential Revision: https://phabricator.services.mozilla.com/D293145

--- gfx/ots/src/cff.cc
+++ gfx/ots/src/cff.cc
@@ -214,7 +214,7 @@ bool ParseDictDataBcd(ots::Buffer &table, std::vector<Operand> &operands) {
 
     // check number format
     uint8_t nibbles[2];
-    nibbles[0] = (nibble & 0xf0) >> 8;
+    nibbles[0] = (nibble & 0xf0) >> 4;
     nibbles[1] = (nibble & 0x0f);
     for (unsigned i = 0; i < 2; ++i) {
       if (nibbles[i] == 0xd) {  // reserved number
@@ -311,7 +311,7 @@ bool ParseDictDataNumber(ots::Buffer &table, uint8_t b0,
     if (!table.ReadU8(&b1)) {
       return OTS_FAILURE();
     }
-    result = -(b0 - 251) * 256 + b1 - 108;
+    result = -(b0 - 251) * 256 - b1 - 108;
   } else {
     return OTS_FAILURE();
   }
