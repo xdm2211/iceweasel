0bc88127358407b17efd67262b2af39f8739ab2b
Timothy Nikkel <tnikkel@gmail.com>
2026-04-09 11:17:02 +0000

    Bug 2029291.  a=diannaS DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D293041

--- gfx/ycbcr/scale_yuv_argb.cpp
+++ gfx/ycbcr/scale_yuv_argb.cpp
@@ -11,6 +11,7 @@
 
 #include "libyuv/scale.h"
 
+#include <algorithm>
 #include <assert.h>
 #include <string.h>
 
@@ -937,7 +938,7 @@ static void YUVToARGBCopy(const uint8_t* src_y, int src_stride_y,
                           YUVColorSpace yuv_color_space)
 {
   YUVBuferIter iter;
-  iter.src_width = src_width;
+  iter.src_width = std::min(src_width, dst_width);
   iter.src_height = src_height;
   iter.src_stride_y = src_stride_y;
   iter.src_stride_u = src_stride_u;
