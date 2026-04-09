424ffa0bcddd005b336d00a20c819248f8af3022
Olli Pettay <Olli.Pettay@helsinki.fi>
2026-04-09 12:42:52 +0000

    Bug 2030123 - return early if ClientNavigateOpChild has lost IPC, r=farre a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D292957

--- dom/clients/manager/ClientNavigateOpChild.cpp
+++ dom/clients/manager/ClientNavigateOpChild.cpp
@@ -279,7 +279,7 @@ RefPtr<ClientOpPromise> ClientNavigateOpChild::DoNavigate(
     return ClientOpPromise::CreateAndReject(result, __func__);
   }
 
-  if (!aProxy->Get()) {
+  if (!aProxy->Get() || !CanSend()) {
     CopyableErrorResult result;
     result.ThrowInvalidStateError("Unknown Client");
     return ClientOpPromise::CreateAndReject(result, __func__);
@@ -315,7 +315,7 @@ void ClientNavigateOpChild::ActorDestroy(ActorDestroyReason aReason) {
 void ClientNavigateOpChild::Init(const ClientNavigateOpConstructorArgs& aArgs,
                                  mozilla::ipc::ActorLifecycleProxy* aProxy) {
   RefPtr<ClientOpPromise> promise = DoNavigate(aArgs, aProxy);
-  if (!aProxy->Get()) {
+  if (!aProxy->Get() || !CanSend()) {
     return;
   }
 
