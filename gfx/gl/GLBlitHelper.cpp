f210fc685d3a0d0e9a8296c08c93610c1316b181
Lee Salzman <lsalzman@mozilla.com>
2026-04-10 16:38:21 +0000

    Bug 2027281.  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D292062
    
    Differential Revision: https://phabricator.services.mozilla.com/D293421

--- gfx/gl/GLBlitHelper.cpp
+++ gfx/gl/GLBlitHelper.cpp
@@ -362,7 +362,7 @@ ScopedSaveMultiTex::ScopedSaveMultiTex(GLContext* const gl,
       mTexUnits(texUnits),
       mTexTarget(texTarget),
       mOldTexUnit(mGL.GetIntAs<GLenum>(LOCAL_GL_ACTIVE_TEXTURE)) {
-  MOZ_RELEASE_ASSERT(texUnits >= 1);
+  MOZ_RELEASE_ASSERT(texUnits >= 1 && texUnits <= std::size(mOldTex));
 
   GLenum texBinding;
   switch (mTexTarget) {
@@ -1631,6 +1631,12 @@ bool GLBlitHelper::Blit(DMABufSurface* surface, const gfx::IntRect& destRect,
 
   const DrawBlitProg::YUVArgs* pYuvArgs = nullptr;
   const auto planes = surface->GetTextureCount();
+
+  // The shaders used below currently only support 1-3 planes.
+  if (planes < 1 || planes > 3) {
+    gfxCriticalError() << "Unexpected DMABuf planes count: " << planes;
+    return false;
+  }
 
   // -
   // Ensure textures for all planes have been created.
