a27b747292f2d1d935229ee782c326514d67cc39
ffxbld <ffxbld@mozilla.com>
2026-04-13 00:58:38 +0000

    No Bug, mozilla-beta repo-update remote-settings tld-suffixes ct-logs -  a=diannaS
    Differential Revision: https://phabricator.services.mozilla.com/D293746

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1781813095000000);
+static const PRTime kCTExpirationTime = INT64_C(1782072355000000);
 
 namespace mozilla::ct {
 
