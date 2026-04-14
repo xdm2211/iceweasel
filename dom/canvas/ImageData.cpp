20edc23bb38d5b9b6c328c2dc9c31f8bb3442f16
Dianna Smith <dsmith@mozilla.com>
2026-04-14 01:57:14 +0000

    Bug 2023814 - ESR115 Harden ImageData structured cloning  a=RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D294069

--- dom/canvas/ImageData.cpp
+++ dom/canvas/ImageData.cpp
@@ -119,10 +119,23 @@ already_AddRefed<ImageData> ImageData::ReadStructuredClone(
       !JS_ReadTypedArray(aReader, &dataArray)) {
     return nullptr;
   }
-  MOZ_ASSERT(dataArray.isObject());
 
-  RefPtr<ImageData> imageData =
-      new ImageData(width, height, dataArray.toObject());
+  JS::Rooted<JSObject*> dataObj(aCx, &dataArray.toObject());
+  RootedSpiderMonkeyInterface<Uint8ClampedArray> data(aCx);
+  if (!data.Init(dataObj)) {
+    return nullptr;
+  }
+
+  data.ComputeState();
+
+  CheckedInt<uint32_t> calculatedLength =
+      CheckedInt<uint32_t>(width) * height * 4;
+  if (!calculatedLength.isValid() ||
+      calculatedLength.value() != data.Length()) {
+    return nullptr;
+  }
+
+  RefPtr<ImageData> imageData = new ImageData(width, height, *dataObj);
   return imageData.forget();
 }
 
